e9a539f8a271e0f06019d48064300bdbfac890dd
viraseptya-adt <viraseptyaa6@gmail.com>
2026-04-22 14:10:57 +0700

    Before_counterpengunjung

---
+++ counter_pengunjung.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pengunjung {
+    public static $jumlah = 0;
+
+    public function __construct() {
+        self::$jumlah++;
+    }
+
+    public static function reset() {
+        self::$jumlah = 0;
+    }
+}
+
+// Membuat 5 objek sesuai perintah
+$p1 = new Pengunjung();
+$p2 = new Pengunjung();
+$p3 = new Pengunjung();
+
+
+echo "Jumlah Pengunjung (Sebelum Reset): " . Pengunjung::$jumlah . "<br>";
+
+// Memanggil method reset
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung (Sesudah Reset): " . Pengunjung::$jumlah;
+
+?>
